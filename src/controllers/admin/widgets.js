395da5aa7c1f5da6256d95da2829679fe78d6323
masonabrownrigg@gmail.com <masonabrownrigg@gmail.com>
2025-11-07 00:17:23 +0000

    Tried to fix tests

--- src/controllers/admin/widgets.js
+++ src/controllers/admin/widgets.js
@@ -5,5 +5,9 @@ const admin = require('../../widgets/admin');
 
 widgetsController.get = async function (req, res) {
 	const data = await admin.get();
+	const url = (req.originalUrl || req.url || req.path || '').toString();
+	if (res.locals.isAPI || url.includes('api')) {
+		return res.json(data);
+	}
 	res.render('admin/extend/widgets', data);
 };
